b82ff214099ce93da114df96082a56f4104eb59b
Uttam Kumar Yadav <yuttam@athenahealth.com>
2021-05-16 14:00:47 +0530

    add bootstrap, validation improvement,  clean the code

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,51 +1,61 @@
 import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Axios from 'axios';
 import OutPut from "./OutPut";
+import {Button, Form} from 'react-bootstrap';
 
 function LandingPage() {
-  const url ="http://localhost:8080/codingasessment/frequencyandsimilarwords"
-  const [invalidForm, setInvalidForm] = useState(true);
+  const url = "http://localhost:8080/codingasessment/frequencyandsimilarwords"
   const [showOutPut, setShowOutPut] = useState(false);
   const [result, setResult] = useState({
-    word:"",
-    notebookText:"",
+    word: "",
+    notebookText: "",
   });
   const [data, setData] = useState({
     word: "",
     notebookText: "",
   })
+
   function handle(e) {
     const newdata = {...data}
     newdata[e.target.id] = e.target.value
     setData(newdata)
-    if(newdata.word && newdata.notebookText){
-      setInvalidForm(false) ;
-    }
-    else {
-      setInvalidForm(true) ;
-    }
   }
+
   function onsubmit(e) {
-   e.preventDefault();
-   Axios.post(url,data)
-    .then(res=>{
+    e.preventDefault();
+    Axios.post(url, data)
+    .then(res => {
       console.log(res.data.response)
       setResult(res.data.response)
       setShowOutPut(true)
     })
   }
-  return(
-    <div>
-      <form onSubmit={(event => onsubmit(event))}>
-        <label>Enter the word </label>
-        <input onChange={(e)=>handle(e)} id="word" value={data.word} type="text"/><br/><br/>
-        <label> Enter the NoteBook Text </label>
-        <textarea onChange={(e)=>handle(e)} id = "notebookText" value={data.notebookText} type="textarea"/><br/><br/>
-        <button disabled={invalidForm}>Submit</button>
-      </form>
-      {showOutPut ? <OutPut result={result}></OutPut>: null}
-    </div>
+
+  return (
+      <div>
+        <Form className='text-form' onSubmit={(event => onsubmit(event))}>
+          <h2>
+            <span>Frequency And Similar Word</span><br/><br/>
+          </h2>
+          <Form.Group >
+            <Form.Label>Enter the Word </Form.Label>
+            <Form.Control placeholder = 'word' onChange={(e) => handle(e)} id="word" value={data.word}
+                          type="text" required/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Notebook Text</Form.Label>
+            <Form.Control placeholder = 'Notebook Text' onChange={(e) => handle(e)} id="notebookText"
+                          value={data.notebookText} as="textarea" required />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+          {showOutPut ? <OutPut result={result}></OutPut> : null}
+        </Form>
+      </div>
   );
 
 }
+
 export default LandingPage
--- src/components/OutPut.js
+++ src/components/OutPut.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {Alert} from "react-bootstrap";
 
 function OutPut(props) {
   return (
-      <div className="OutPut">
-        <div><p>The Frequency of word <b>{props.result.word}</b> is: <b>{props.result.wordFrequency}</b></p></div>
-        <div><p>Similar words are in note: <b>{props.result.similarWords.join(", ")}</b></p></div>
+      <div className="OutPut"><br/>
+        <Alert variant="success">
+          <p>The Frequency of word <b>{props.result.word}</b> is: <b>{props.result.wordFrequency}</b></p>
+        </Alert>
+        <Alert variant="success">
+          <p>Similar words are in note: <b>{props.result.similarWords.join(", ")}</b></p>
+        </Alert>
       </div>
   );
 }
